ffd2aaca4fde17a82cc047f93c8d812acdfcd61c
Jahmilli <sebastian.southern@gmail.com>
2019-11-16 20:44:35 +1100

    deleted images as they are now in S3

--- src/components/pages/Projects/Projects.tsx
+++ src/components/pages/Projects/Projects.tsx
@@ -3,7 +3,6 @@ import styles from "./Projects.module.css";
 import ProjectItem from "./ProjectItem";
 import Title from '../../presentational/Title/Title';
 import ProjectPreview from '../../presentational/ProjectPreview/ProjectPreview';
-import VoluntimeImage from "../../../images/voluntime-home-screen.png";
 
 type ProjectsProps = {
   position: string;
@@ -40,6 +39,13 @@ const Projects: React.FunctionComponent<ProjectsProps> = ({ position, handleSetI
   return (
     <div className={styles.lockup}>
       <Title title="Projects" position={position} handleSetInactive={handleSetInactive} />
+      <p>
+        Below is a collection of some of the projects I have been quite happy with. These are a collection of works that I have
+        done both at university and outside of university. 
+        <br />
+        Each project will have a link to the source code on Github as well as 
+        a description of what the application was, how it was built and some of the key learnings from it.
+      </p>
       <div className={styles.gallery}>
         <ProjectPreview
           title="Voluntime"
@@ -48,35 +54,50 @@ const Projects: React.FunctionComponent<ProjectsProps> = ({ position, handleSetI
           gitURL="https://github.com/Jahmilli/Voluntime"
           images={[
             {
-              src: VoluntimeImage,
-              alt: "Voluntime"
-            }
+              src: "https://seb-southern-portfolio.s3.ap-northeast-2.amazonaws.com/voluntime-home-page.png",
+              alt: "Voluntime Home Page"
+            },
+            {
+              src: "https://seb-southern-portfolio.s3.ap-northeast-2.amazonaws.com/voluntime-create-event.png",
+              alt: "Voluntime Create Event Page"
+            },
+            {
+              src: "https://seb-southern-portfolio.s3.ap-northeast-2.amazonaws.com/voluntime-events-map.png",
+              alt: "Voluntime Events Map"
+            },
+            {
+              src: "https://seb-southern-portfolio.s3.ap-northeast-2.amazonaws.com/voluntime-review-registration.png",
+              alt: "Voluntime Review Registration Page"
+            },
+            {
+              src: "https://seb-southern-portfolio.s3.ap-northeast-2.amazonaws.com/voluntime-upcoming-events.png",
+              alt: "Voluntime Upcoming Events Page"
+            },
           ]}
           handleItemClick={handleItemClick}
         />
         <ProjectPreview
-          title="Voluntime"
+          title="SES Health Application"
           date="10/10/2019"
-          description="An Android application built as part of the first Software Engineering Studio subject at UTS"
-          gitURL="https://github.com/Jahmilli/Voluntime"
+          description="An Android application built as part of the second Software Engineering Studio subject at UTS"
+          gitURL="https://github.com/Jahmilli/SESHealth"
           images={[
             {
-              src: VoluntimeImage,
-              alt: "Voluntime"
-            }
-          ]}
-          handleItemClick={handleItemClick}
-        />
-        <ProjectPreview
-          title="Voluntime"
-          date="10/10/2019"
-          description="An Android application built as part of the first Software Engineering Studio subject at UTS"
-          gitURL="https://github.com/Jahmilli/Voluntime"
-          images={[
+              src: "https://seb-southern-portfolio.s3.ap-northeast-2.amazonaws.com/seshealth-home-page.png",
+              alt: "SES Health Home Page"
+            },
             {
-              src: VoluntimeImage,
-              alt: "Voluntime"
-            }
+              src: "https://seb-southern-portfolio.s3.ap-northeast-2.amazonaws.com/seshealth-doctor-key.png",
+              alt: "SES Health Doctor Key"
+            },
+            {
+              src: "https://seb-southern-portfolio.s3.ap-northeast-2.amazonaws.com/seshealth-practices-map.png",
+              alt: "SES Health Practices Map"
+            },
+            {
+              src: "https://seb-southern-portfolio.s3.ap-northeast-2.amazonaws.com/seshealth-send-file.png",
+              alt: "SES Health Send File"
+            },
           ]}
           handleItemClick={handleItemClick}
         />
